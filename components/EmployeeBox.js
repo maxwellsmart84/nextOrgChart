c8057076245ef415858b8313a986bba50f59c161
Maxwell Krause <maxwell.l.krause@gmail.com>
2018-02-01 08:54:30 -0500

    forgot to save empBox

--- components/EmployeeBox.js
+++ components/EmployeeBox.js
@@ -1,8 +1,13 @@
 import Link from 'next/link';
+import EmployeeBoxHeader from './EmployeeBoxHeader'
 
 
 const styles = {
 
 }
 
-const EmployeeBox
+const EmployeeBox = (props) => (
+  <div>
+    <EmployeeBoxHeader />
+  </div>
+)
